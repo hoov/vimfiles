dbeb56fcc46abdb932ff9a54a817fa321290ff9f
Thomas Hoover <thoover@alum.mit.edu>
2010-07-11 17:36:48 -0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+for f in gvimrc vimrc; do
+    ln -sf `pwd`/$f ~/.$f
+done
+
+if [[ ! -e ~/.vim ]]; then
+  mkdir -p ~/.vim
+fi
+
+for f in autoload bundle; do
+    ln -sf `pwd`/$f ~/.vim/$f
+done
